f0783ba18265a9132c3fc8097aaf25a05801fab1
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-15 03:23:31 -0400

    Add favicon.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { routes } from './config/routes'
 
+require('file?name=favicon.ico!../assets/favicon.ico')
+
 console.log('Puka running in environment:', process.env.NODE_ENV);
 ReactDOM.render(routes, document.getElementById('app'))
