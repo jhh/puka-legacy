f83d488818c390928052cf10a9edb058f8b859b4
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-16 10:39:40 -0400

    Bookmarks to tags target _self.

--- src/components/dev-bookmarks.jsx
+++ src/components/dev-bookmarks.jsx
@@ -1,43 +1,14 @@
 import React from 'react'
 
-const BOOKMARKS = [
-  {
-    title: 'React',
-    url: '/tag/react'
-  },
-  {
-    title: 'Redux',
-    url: '/tag/redux'
-  },
-  {
-    title: 'Mozilla Developer Network',
-    url: 'https://developer.mozilla.org/en-US/'
-  },
-  {
-    title: 'Bootstrap 4',
-    url: 'http://v4-alpha.getbootstrap.com/'
-  },
-  {
-    title: 'HTML5 Please',
-    url: 'http://html5please.com/'
-  },
-  {
-    title: 'Axios',
-    url: 'https://github.com/mzabriskie/axios'
-  },
-  {
-    title: 'React.js Program',
-    url: 'https://github.com/reactjsprogram'
-  },
-  {
-    title: 'Chrome DevTools',
-    url: 'https://developers.google.com/web/tools/chrome-devtools/'
-  }
-]
+const BOOKMARKS = require('json!./dev-bookmarks.json')
 
 export const DevBookmarks = (props) => {
   let bookmarks = BOOKMARKS.map((bm, i) => {
-    return <li key={i}><a href={bm.url} target='_blank'>{bm.title}</a></li>
+    return <li key={i}>
+      <a href={bm.url} target={bm.url.startsWith('/tag') ? '_self' : '_blank'}>
+        {bm.title}
+      </a>
+    </li>
   })
   return (<ul className='list-unstyled'>{bookmarks}</ul>
   )
