fac2ad7aeee7ab000f802fa1a5aefc80523637ba
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-19 15:34:11 -0400

    API requests should include session cookie.

--- src/util/pukaAPI.js
+++ src/util/pukaAPI.js
@@ -2,22 +2,31 @@ import fetch from 'isomorphic-fetch';
 
 const HOST = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:9292';
 
-function mapResponse(json) {
+// TODO: check for JSON API errors
+function mapResponse(response) {
   const map = new Map();
-  for (const item of json.data) {
-    map.set(item.id, item.attributes);
+  try {
+    for (const item of response.data) {
+      map.set(item.id, item.attributes);
+    }
+  } catch (e) {
+    Promise.reject(new Error(e.message));
   }
-  return map;
+  return Promise.resolve(map);
+}
+
+function doFetch(url) {
+  return fetch(url, { redirect: 'error', credentials: 'include' }).then(response => {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response.json());
+    }
+    return Promise.reject(new Error(response.statusText));
+  })
+  .then(mapResponse);
 }
 
 export default {
-  getBookmarks: () => fetch(`${HOST}/api/bookmarks?page[limit]=100`)
-    .then(response => response.json())
-    .then(mapResponse),
-  getBookmarksByTag: (tag) => fetch(`${HOST}/api/bookmarks?page[limit]=100&filter[tag]=${tag}`)
-    .then(response => response.json())
-    .then(mapResponse),
-  getBookmark: (id) => fetch(`${HOST}/api/bookmarks/${id}`)
-  .then(response => response.json())
-  .then(mapResponse),
+  getBookmarks: () => doFetch(`${HOST}/api/bookmarks?page[limit]=100`),
+  getBookmarksByTag: (tag) => doFetch(`${HOST}/api/bookmarks?page[limit]=100&filter[tag]=${tag}`),
+  getBookmark: (id) => doFetch(`${HOST}/api/bookmarks/${id}`),
 };
