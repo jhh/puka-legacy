e7f632ee65e5b9c61ef890276e8425c296bdecab
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-22 07:40:29 -0400

    Move styles to CSS Module.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,17 +1,12 @@
 import React, { PropTypes } from 'react';
 import { DevBookmarks } from './dev-bookmarks';
 import { Link } from 'react-router';
-
-const headingStyle = {
-  paddingBottom: '9px',
-  margin: '40px 0 20px',
-  borderBottom: '1px solid #eee',
-};
+import styles from '../styles/main.css';
 
 export const Main = (props) => (
   <main className="container">
-    <div style={headingStyle}>
-      <h1><Link to="/" style={{ color: '#000', textDecoration: 'none' }}>Puka</Link></h1>
+    <div className={styles.heading}>
+      <h1><Link to="/" className={styles.brand}>Puka</Link></h1>
     </div>
     <div className="row">
       <div className="col-md-8">
