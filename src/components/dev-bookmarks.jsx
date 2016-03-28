a1fcfa04051fa638837aed9c3e00352bcbe38a72
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-28 15:01:13 -0400

    Use router for internal links.

--- src/components/dev-bookmarks.jsx
+++ src/components/dev-bookmarks.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const BOOKMARKS = require('json!../../assets/dev-bookmarks.json');
+const STYLE = { color: '#00b7ff' };
 
 export const DevBookmarks = () => {
   const bookmarks = BOOKMARKS.map((bm, i) => {
     const isTag = bm.url.startsWith('/tag');
+    const link = isTag
+      ? <Link to={bm.url} style={STYLE}>{bm.title}</Link>
+      : <a href={bm.url} style={STYLE} target="_blank">{bm.title}</a>;
     return (<li key={i}>
       <span
         style={{ color: '#bbb', paddingRight: 5 }}
         className={isTag ? 'octicon octicon-three-bars' : 'octicon octicon-link-external'}
       />
-      <a href={bm.url} style={{ color: '#00b7ff' }} target={ isTag ? '_self' : '_blank'}>
-        {bm.title}
-      </a>
+      {link}
     </li>);
   });
 
