da67c29a7b30ba077d06741a4e1e8afc53c7711c
Jeff Hutchison <jeff@jeffhutchison.com>
2016-04-03 17:38:20 -0400

    Refactor Bookmark into component dir.

--- src/components/bookmark/index.jsx
+++ src/components/bookmark/index.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import moment from 'moment';
-import styles from '../styles/bookmark.css';
+import styles from './styles.css';
 
 function formatTags(tags) {
   return tags.map((tag, i) =>
