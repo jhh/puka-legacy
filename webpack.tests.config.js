d01c1d0258c331d8b320c4021a5aeeb9e394f36d
Jeff Hutchison <jeff@jeffhutchison.com>
2016-04-04 13:33:05 -0400

    Implement in-browser testing.

---
+++ webpack.tests.config.js
@@ -0,0 +1,42 @@
+const hostname = 'localhost';
+const port = 8000;
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: 'mocha!./test/index.js',
+  output: {
+    filename: 'test.build.js',
+    path: 'test/',
+    publicPath: `http://${hostname}:${port}/test`,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: [
+          /node_modules/,
+          /public/,
+        ],
+      },
+      {
+        test: /(\.css|\.less)$/,
+        loader: 'null-loader',
+        exclude: [
+          /build/,
+        ],
+      },
+      {
+        test: /(\.jpg|\.jpeg|\.png|\.gif)$/,
+        loader: 'null-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devServer: {
+    host: hostname,
+    port,
+  },
+};
