28fd2b89c243fb5b9de40936cd4357dd066c16dd
Jeff Hutchison <jeff@jeffhutchison.com>
2016-04-27 08:49:41 -0400

    add a quick db query action.

--- cmd/puka/main.go
+++ cmd/puka/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/codegangsta/cli"
+	"github.com/jhh/puka"
 
 	"gopkg.in/mgo.v2"
 )
@@ -13,8 +15,14 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "puka"
 	app.Usage = "manage puka data"
+	app.Version = "0.0.2"
 
 	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "url",
+			Value: "mongodb://localhost/test",
+			Usage: "mongo url connection string",
+		},
 		cli.StringFlag{
 			Name:  "database, db",
 			Value: "test",
@@ -27,19 +35,33 @@ func main() {
 		},
 	}
 
-	app.Action = UpdateBookmarks
+	app.Commands = []cli.Command{
+		{
+			Name:   "update",
+			Usage:  "update the document schema",
+			Action: UpdateBookmarks,
+		},
+		{
+			Name:   "bookmark",
+			Usage:  "print one bookmark",
+			Action: PrintOne,
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		log.Println("DEBUG", c.String("url"), c.String("database"), c.String("collection"))
+	}
 	app.Run(os.Args)
 }
 
 // UpdateBookmarks updates the field names.
 func UpdateBookmarks(c *cli.Context) {
-	session, err := mgo.Dial("localhost")
+	session, err := mgo.Dial(c.GlobalString("url"))
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("FATAL", err)
 	}
 	defer session.Close()
 
-	col := session.DB(c.String("database")).C(c.String("collection"))
+	col := session.DB(c.GlobalString("database")).C(c.GlobalString("collection"))
 
 	result := LegacyBookmark{}
 	iter := col.Find(nil).Iter()
@@ -50,6 +72,26 @@ func UpdateBookmarks(c *cli.Context) {
 		}
 	}
 	if err = iter.Close(); err != nil {
-		log.Fatalln(err)
+		log.Fatalln("FATAL", err)
 	}
+}
+
+// PrintOne will retrieve a bookmark and print to std out.
+func PrintOne(c *cli.Context) {
+	session, err := mgo.Dial(c.GlobalString("url"))
+	if err != nil {
+		log.Println(c.GlobalString("url"))
+		log.Fatalln("FATAL", err)
+	}
+	defer session.Close()
+
+	col := session.DB(c.GlobalString("database")).C(c.GlobalString("collection"))
+
+	result := puka.Bookmark{}
+	err = col.Find(nil).One(&result)
+	if err != nil {
+		log.Println("DEBUG", c.GlobalString("url"), c.GlobalString("database"), c.GlobalString("collection"))
+		log.Fatalln("FATAL", err)
+	}
+	fmt.Println(result)
 }
