b219952a7c6a4aab01f876a1e080d975c652b0b4
Jeff Hutchison <jeff@jeffhutchison.com>
2016-04-29 14:52:23 -0400

    Simplify imports.

--- cmd/puka-web/main.go
+++ cmd/puka-web/main.go
@@ -8,11 +8,10 @@ import (
 	"strconv"
 
 	"github.com/jhh/puka/model"
+	"github.com/jhh/puka/resolver"
 	"github.com/jhh/puka/resource"
 	"github.com/jhh/puka/storage"
-	"github.com/julienschmidt/httprouter"
 	"github.com/manyminds/api2go"
-	"github.com/manyminds/api2go/examples/resolver"
 )
 
 func main() {
@@ -25,11 +24,5 @@ func main() {
 	api.AddResource(model.Bookmark{}, resource.BookmarkResource{BookmarkStorage: bookmarkStorage})
 
 	fmt.Printf("Listening on :%d", port)
-	handler := api.Handler().(*httprouter.Router)
-	// It is also possible to get the instance of julienschmidt/httprouter and add more custom routes!
-	handler.GET("/hello-world", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprint(w, "Hello World!\n")
-	})
-
-	http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), api.Handler())
 }
