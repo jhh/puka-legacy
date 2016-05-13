88f2755873d0b332cac3e26fbd5654226ed11f2d
Jeff Hutchison <jeff@jeffhutchison.com>
2016-05-12 20:22:26 -0400

    Stash the CORS header stuff.

---
+++ lib/middleware/cors.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/manyminds/api2go"
+)
+
+func CORS(c api2go.APIContexter, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	// TODO: Check Access-Control-Request-Headers, Access-Control-Request-Method
+	w.Header().Set("Access-Control-Allow-Headers", "accept, content-type")
+}
