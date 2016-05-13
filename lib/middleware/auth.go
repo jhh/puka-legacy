e83d06df0bf0e9ef0d984f0fcb80029f0eb5f8c5
Jeff Hutchison <jeff@jeffhutchison.com>
2016-05-12 21:37:01 -0400

    Stub out.

--- lib/middleware/auth.go
+++ lib/middleware/auth.go
@@ -8,22 +8,4 @@ import (
 
 // Authenticate puts error in context if not authenticated.
 func Authenticate(c api2go.APIContexter, w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	// TODO: Check Access-Control-Request-Headers, Access-Control-Request-Method
-	w.Header().Set("Access-Control-Allow-Headers", "accept, content-type")
-	// p := r.FormValue("foo")
-	// if p != "bar" {
-	// 	expiration := time.Now().Add(5 * time.Minute)
-	// 	cookie := http.Cookie{
-	// 		Name:     "username",
-	// 		Value:    "jhh",
-	// 		Expires:  expiration,
-	// 		HttpOnly: true,
-	// 	}
-	// 	http.SetCookie(w, &cookie)
-	// 	return
-	// }
-	// err := api2go.NewHTTPError(errors.New("auth check failed"), "not authorized", http.StatusForbidden)
-	// c.Set("error", err)
 }
