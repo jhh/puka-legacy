3e4ff986873b3e79febe330ac0ce9ad7f0c44615
Jeff Hutchison <jeff@jeffhutchison.com>
2016-05-12 21:19:58 -0400

    Add doc comment.

--- lib/middleware/cors.go
+++ lib/middleware/cors.go
@@ -6,6 +6,7 @@ import (
 	"github.com/manyminds/api2go"
 )
 
+// CORS sets the neccessary headers.
 func CORS(c api2go.APIContexter, w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
